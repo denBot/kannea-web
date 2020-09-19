e23a4597e7db9d6d3af1554b78f627749fd4a7e1
Daniel A <den.xyz@pm.me>
2020-09-19 15:57:17 +0100

    Updated .gitignore to include .adminbro directory

--- .adminbro/.entry.js
+++
@@ -1,5 +0,0 @@
-AdminBro.UserComponents = {}
-import Component1 from '../server/admin/components/upload/avatar.edit'
-AdminBro.UserComponents.Component1 = Component1
-import Component2 from '../server/admin/components/upload/avatar.list'
-AdminBro.UserComponents.Component2 = Component2
