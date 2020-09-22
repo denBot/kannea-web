a51cfe74384e8a2698da84487e29be8c9e50e4a1
Daniel A <den.xyz@pm.me>
2020-09-22 15:34:08 +0100

    Changed SettingPage to a stateful class component

--- src/server/admin/components/pages/settings.page.tsx
+++ src/server/admin/components/pages/settings.page.tsx
@@ -1,12 +1,35 @@
-import React from 'react'
 import { Label, Box } from 'admin-bro'
+import React, { Component } from 'react'; // let's also import Component
 
-const Page = () => {
-  return (
-    <Box>
-      <Label>Settings Page</Label>
-    </Box>
-  )
+type settingsState = {
+  settings: any,
+  isLoading: boolean
 }
 
-export default Page
+export class SettingsPage extends Component<{}, settingsState> {
+
+  constructor(props: any) {
+    super(props)
+    this.state = {
+      settings: null,
+      isLoading: true
+    }
+  }
+
+  getSettings() {
+    this.setState({
+      settings: {someSetting: 1},
+      isLoading: false
+    });
+  }
+
+  componentDidMount() {
+    this.getSettings()
+  }
+
+  render() {
+    return <p>The current time is {JSON.stringify(this.state.settings)}</p>
+  }
+}
+
+export default SettingsPage
